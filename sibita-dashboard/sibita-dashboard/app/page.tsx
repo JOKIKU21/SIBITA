bf9bbd559e2a8f5ba9d1ef4b4388f0be5c3be450
nakslop <fatihali050804@gmail.com>
2026-07-03 15:46:11 +0700

    Remove nested sibita-dashboard folder

--- sibita-dashboard/sibita-dashboard/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-// Sesuai permintaan: tidak ada landing page atau halaman login.
-// Membuka "/" langsung membawa mahasiswa ke dashboard.
-export default function RootPage() {
-  redirect("/dashboard");
-}
